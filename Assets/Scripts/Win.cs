dffbb8e4284bd3ea77bdb62ea515fefcfef11173
Ivan Mudrák <xmudra03@vutbr.cz>
2023-01-18 18:49:24 +0100

    win + win music

--- Assets/Scripts/Win.cs
+++ Assets/Scripts/Win.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Win : MonoBehaviour
 {
     private bool triggered;
+    public GameObject WinCanvas;
+    public AudioSource BGMusic;
+    public AudioSource BGWinMusic;
 
     private void Start()
     {
@@ -15,8 +18,11 @@ public class Win : MonoBehaviour
     {
         if (triggered)
             return;
+
+        WinCanvas.SetActive(true);
+        BGMusic.Stop();
+        BGWinMusic.Play();
         
         triggered = true;
-        Debug.Log("You win");
     }
 }
