c80324d3f2b23ab116ef9bc934e892a2a96d9f7b
Michaela Macková <michaela.mackovaa@gmail.com>
2023-01-18 01:17:18 +0100

    Sounds

---
+++ Assets/Scripts/SoundEffects.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffects : MonoBehaviour
+{
+
+    public AudioSource runSoundEffect;
+    public AudioSource attackSoundEffect;
+    public AudioSource jumpSoundEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RunSound(){
+        runSoundEffect.Play();
+    }
+
+    public void AttackSound(){
+        attackSoundEffect.Play();
+    }
+
+    public void JumpSound(){
+        jumpSoundEffect.Play();
+    }
+}
